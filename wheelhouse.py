b82a74182477422a95145ff31ef0c1e7026d880d
Petr Michalec <epcim@apealive.net>
2018-02-15 00:08:22 +0100

    WIP - New version that expect only yaml  on input and parse args

--- wheelhouse.py
+++ wheelhouse.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python
 
+# Execute demo:
+# docker run -v $PWD:/wh -ti epcim/salt-formulas /bin/bash
+# docker run -v $PWD:/wh -ti epcim/salt-formulas:ubuntu-bionic-saltformulas-2018.2 /bin/bash
+# install influxdb:
+#   curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+#   source /etc/os-release
+#   echo "deb https://repos.influxdata.com/${ID,,} ${VERSION_CODENAME:- stretch} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+#   sudo apt-get update && sudo apt-get install influxdb
+#   influxd &
+# /wh/wheelhouse.py
+
+
 import os
 import sys
 import salt.client
@@ -8,14 +20,198 @@ import salt.output
 from glob import glob
 import ruamel.yaml
 import pprint
+import argparse
+import select
+
+
+class Toolbox():
+
+    @staticmethod
+    def arg2config(arg):
+        """
+        Example:
+          --logging-severity=ABC
+          --maps-list-values=A,B,C
+        becomes dict like:
+          logging:
+            severity: ABC
+          maps:
+            list:
+              values: [A, B , C]
+        """
+        t = arg.split('=')
+        v = t[1:] # value
+        if len(v) == 0:
+          # no value -> boolean switch
+          v = True
+        else:
+          v = v[0].split(',')
+          v = v if len(v) > 1 else ''.join(v).strip()
+        a = list(reversed(t[0].split('-')))
+        r = {a[0]: v}
+        for a in a[1:]:
+          r = {a: r}
+        return r
+
+    @staticmethod
+    def merge_dict(d1, d2):
+      for k in d2:
+        if k in d1 and isinstance(d1[k], dict) and isinstance(d2[k], dict):
+            Toolbox.merge_dict(d1[k], d2[k])
+        else:
+            d1[k] = d2[k]
+
+    @staticmethod
+    def argparser():
+        parser = argparse.ArgumentParser(description=
+                                         "Wheelhouse script.\n"
+                                         "Usage: `cat wheelhouse.yaml | wheelhouse.py [args]`")
+        wh = parser.add_argument_group('Choosing what you want to execute')
+        wh.add_argument('jobs', nargs="*",
+                        help='Job names to execute')
+        wh.add_argument('-c', '--config',
+                        default=None,
+                        help='YAML file containing run instructions')
+        #wh.add_argument('-j', '--jobs',
+        #                default=None,
+        #                help='Comma separated list of job names to execute')
+        wh.add_argument('-t', '--test', action='store_true',
+                        help='Run embedded pillar validation and tests')
+        wh.add_argument('--dry', action='store_true',
+                        help='Execute engine in a "dry" mode')
+        at = parser.add_argument_group('Any extra attributes will ones spec. in config file)')
+        at.add_argument('-e', '--engine',
+                        default=None,
+                        help='Wheel engine to use.')
+        # technically any string value on wheelhouse config can be overriden
+        at.add_argument('extra', nargs=argparse.REMAINDER)
+        return parser
+
+
+    @staticmethod
+    def testconfig():
+        return ruamel.yaml.YAML().load("""\
+            enabled: true
+            engine: salt
+            image:  tcpcloud/salt-formulas
+            logging:
+              severity: info
+            job:
+                initdb:
+                    wheel:
+                        - initdb
+                    logging:
+                        severity: debug
+                cron_data_prunning:
+                    wheel:
+                        - minion_influxdb_config
+                        - delete_data
+                dummy_test:
+                    wheel:
+                        - minion_influxdb_config
+                        - dummy_direct_sls_invocation
+            pillar:
+                salt:
+                  minion:
+                      config:
+                        # This section is only needed if salt state ``influxdb_continuous_query.present`` is used
+                        influxdb:
+                          host: localhost
+                          port: 8086
+                influxdb:
+                    client:
+                        enabled: true
+                        server:
+                          protocol: http
+                          host: localhost
+                          port: 8086
+                          user: admin
+                          password: admin
+                        user:
+                          1:
+                            name: fluentd
+                            password: password
+                            enabled: true
+                            admin: true
+                        database:
+                            1:
+                                name: h2o_measurement
+                                enabled: true
+                                retention_policy:
+                                - name: rp_a_month
+                                  duration: 30d
+                                  replication: 1
+                                  is_default: true
+                                continuous_query:
+                                    cq_avg_bus_passengers: >-
+                                        SELECT mean("h2o_quality") INTO "h2o_measurement"."rp_a_month"."average_quality" FROM "bus_data" GROUP BY time(1h)
+                                    cq_br_downsample_all_hourly: >-
+                                        SELECT mean(*) INTO "h2o_measurement"."ds_all_mean_h".:MEASUREMENT FROM /.*/ GROUP BY time(60m),*
+                                query:
+                                    drop_h2o: >-
+                                        DROP MEASUREMENT h2o_measurement
+                                    delete_h2o: >-
+                                        DELETE FROM "h2o_measurement" WHERE type='h2o'
+                        grant:
+                            fluentd_h2o_measurement:
+                                enabled: true
+                                user: fluentd
+                                database: h2o_measurement
+                                privilege: all
+            wheel:
+                initdb:
+                    state.apply:
+                        - influxdb.client
+                delete_data:
+                    # Call formula state sls NS by an ID
+                    state.sls_id:
+                        - delete_h2o
+                        - influxdb.query
+                minion_influxdb_config:
+                    state.apply:
+                        /etc/salt/minion:
+                            file.serialize:
+                            - dataset_pillar:  salt:minion:config
+                            - formatter:       yaml
+                            - merge_if_exists: True
+                            - makedirs: True
+                dummy_direct_sls_invocation:
+                    state.apply:
+                        make_dir:
+                            file.directory:
+                            - name: /var/log/influxdb
+                            - makedirs: True
+                            - mode: 755
+                        #create_dummy_database:
+                        #    influxdb_database.present:
+                        #        - name: dummy1
+                        #set_a_year_retention_db_metrics:
+                        #    influxdb_retention_policy.present:
+                        #        - name: a_year
+                        #        - database: dummy1
+                        #        - duration: 52w
+                        #        - retention: 1
+                        #        - default: false
+                        #        #- host: localhost
+                        #        #- port: 8086
+                        #insert_dummy_data:
+                        #    module.run:
+                        #      influxdb.query:
+                        #        - database: h2o_measurement
+                        #        - query: INSERT treasures,captain_id=pirate_king value=2
+                        #set_continuous_queries:
+                        #    influxdb_continuous_query.present:
+                        #       ...
+                        #       ...
+        """)
 
 
 class Wheel:
 
-    def __init__(self, config, recipe=[]):
+    def __init__(self, config, jobs=[]):
         self.logseverity = {'error': 1, 'info': 2, 'debug': 3}
         self.config = config
-        self.recipe = recipe
+        self.jobs = jobs
 
     def log(self, msg, severity='info', level=0):
         if self.logseverity[severity] <= self.logseverity[self.config.get('logging', {}).get('severity', 'info')]:
@@ -28,11 +224,11 @@ class Wheel:
         wheelhouse:
           job:
             <job_name>:
-              recipe:
+              wheel:
                 - <wheel_name>
         """
 
-        for job_nm in self.recipe:
+        for job_nm in self.jobs:
             job = self.config['job'][job_nm]
 
             self.log('Job: {}'.format(job_nm))
@@ -69,9 +265,9 @@ class Wheel:
 
 class SaltWheel(Wheel):
 
-    def __init__(self, config, recipe=None):
-        Wheel.__init__(self, config, recipe=recipe)
-        self.salt_opts = {'state-output': 'changes', 'log-severity': 'info', 'with_grains': True, 'test': False}
+    def __init__(self, config, jobs=None, dry=False):
+        Wheel.__init__(self, config, jobs=jobs)
+        self.salt_opts = {'state-output': 'changes', 'log-severity': 'info', 'with_grains': True, 'test': dry}
         self.salt_config = {}
 
     def client(self):
@@ -129,7 +325,7 @@ class SaltWheel(Wheel):
                 elif isinstance(values, ruamel.yaml.comments.CommentedMap) or isinstance(values, dict):  # RAW SLS FILE ON VALUES
                     with open('top.sls', 'w') as out:
                         out.write(''.join((
-                            "base:\n",
+                                "base:\n",
                             "  '*':\n",
                             "     - {}\n".format(wheel_nm)
                         )))
@@ -141,152 +337,79 @@ class SaltWheel(Wheel):
             kwargs = self.safeMergeDict(self.salt_opts, wheel.get('config', {}).get('salt', {}).get('opts', {}))
             kwargs = self.safeMergeDict(kwargs, pillar)
 
-            ret = salt_c.cmd(fn, *args, **kwargs)
+            ret = None
+            try:
+                ret = salt_c.cmd(fn, *args, **kwargs)
 
-            # workaround, check for failed states:
-            _retcode = 0
-            for v in ret.values():
-                if not v.get('result', True):
-                    _retcode = 1
-                    break
+                # workaround, check for failed states:
+                _retcode = 0
+                for v in ret.values():
+                    if not v.get('result', True):
+                        _retcode = 1
+                        break
 
-            salt.output.display_output(
-                {'local': ret},
-                out=ret.get('out', 'highstate'),
-                opts=self.salt_config,
-                _retcode=ret.get('retcode', _retcode))
+                salt.output.display_output(
+                    {'local': ret},
+                    out=ret.get('out', 'highstate'),
+                    opts=self.salt_config,
+                    _retcode=ret.get('retcode', _retcode))
+
+            except Exception as e:
+                self.log('Exception: {} for a ret object: {} '.format(e, ret))
+                pp = pprint.PrettyPrinter(indent=2, width=80)
+                pp.pprint(ret)
+                sys.exit(1)
 
             if self.salt_config.get('retcode_passthrough', False) and ret.get('retcode', _retcode) != 0:
                 sys.exit(ret.get('retcode', _retcode))
 
 
-# For testing:
+# Main for ad-hoc, free hands execution:
 if __name__ == '__main__':
-    pp = pprint.PrettyPrinter(indent=2, width=80)
-    config = ruamel.yaml.YAML().load("""\
-        enabled: true
-        engine: salt
-        image:  tcpcloud/salt-formulas
-        logging:
-          severity: info
-        job:
-            initdb:
-                wheel:
-                    - initdb
-                logging:
-                    severity: debug
-            cron_data_prunning:
-                wheel:
-                    - minion_influxdb_config
-                    - delete_data
-            dummy_test:
-                wheel:
-                    - minion_influxdb_config
-                    - dummy_direct_sls_invocation
-        pillar:
-            salt:
-              minion:
-                  config:
-                    # This section is only needed if salt state ``influxdb_continuous_query.present`` is used
-                    influxdb:
-                      host: localhost
-                      port: 8086
-            influxdb:
-                client:
-                    enabled: true
-                    server:
-                      protocol: http
-                      host: localhost
-                      port: 8086
-                      user: admin
-                      password: admin
-                    user:
-                      1:
-                        name: fluentd
-                        password: password
-                        enabled: true
-                        admin: true
-                    database:
-                        1:
-                            name: h2o_measurement
-                            enabled: true
-                            retention_policy:
-                            - name: rp_a_month
-                              duration: 30d
-                              replication: 1
-                              is_default: true
-                            continuous_query:
-                                cq_avg_bus_passengers: >-
-                                    SELECT mean("h2o_quality") INTO "h2o_measurement"."rp_a_month"."average_quality" FROM "bus_data" GROUP BY time(1h)
-                                cq_br_downsample_all_hourly: >-
-                                    SELECT mean(*) INTO "h2o_measurement"."ds_all_mean_h".:MEASUREMENT FROM /.*/ GROUP BY time(60m),*
-                            query:
-                                drop_h2o: >-
-                                    DROP MEASUREMENT h2o_measurement
-                                delete_h2o: >-
-                                    DELETE FROM "h2o_measurement" WHERE type='h2o'
-                    grant:
-                        fluentd_h2o_measurement:
-                            enabled: true
-                            user: fluentd
-                            database: h2o_measurement
-                            privilege: all
-        wheel:
-            initdb:
-                state.apply:
-                    - influxdb.client
-            delete_data:
-                # Call formula state sls NS by an ID
-                state.sls_id:
-                    - delete_h2o
-                    - influxdb.query
-            minion_influxdb_config:
-                state.apply:
-                    /etc/salt/minion:
-                        file.serialize:
-                        - dataset_pillar:  salt:minion:config
-                        - formatter:       yaml
-                        - merge_if_exists: True
-                        - makedirs: True
-            dummy_direct_sls_invocation:
-                state.apply:
-                    make_dir:
-                        file.directory:
-                        - name: /var/log/influxdb
-                        - makedirs: True
-                        - mode: 755
-                    #create_dummy_database:
-                    #    influxdb_database.present:
-                    #        - name: dummy1
-                    #set_a_year_retention_db_metrics:
-                    #    influxdb_retention_policy.present:
-                    #        - name: a_year
-                    #        - database: dummy1
-                    #        - duration: 52w
-                    #        - retention: 1
-                    #        - default: false
-                    #        #- host: localhost
-                    #        #- port: 8086
-                    #insert_dummy_data:
-                    #    module.run:
-                    #      influxdb.query:
-                    #        - database: h2o_measurement
-                    #        - query: INSERT treasures,captain_id=pirate_king value=2
-                    #set_continuous_queries:
-                    #    influxdb_continuous_query.present:
-                    #       ...
-                    #       ...
 
-    """)
-    recipe = ['initdb', 'dummy_test']
-    SaltWheel(config, recipe=recipe).runner()
+    parser = Toolbox.argparser()
+    args, extra = parser.parse_known_args()
 
-# Execute tests:
-# docker run -v $PWD:/wh -ti epcim/salt-formulas /bin/bash
-# install influxdb:
-#   curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
-#   source /etc/os-release
-#   echo "deb https://repos.influxdata.com/${ID,,} ${VERSION_CODENAME:- stretch} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
-#   sudo apt-get update && sudo apt-get install influxdb
-#   influxd &
-# /wh/wheelhouse.py
+    # construct simple override from extra args
+    config_override = {}
+    # implement poor introspection of extra attributes.
+    for a in ' '.join(extra).split('--'):
+        if a and a.strip() != '':
+            Toolbox.merge_dict(config_override, Toolbox.arg2config(a))
+
+    jobs = []
+
+    # apply args
+    if args.engine:
+        config_override['engine'] = args.engine
+    if args.test:
+        # run embedded dummy test
+        jobs = ['initdb', 'dummy_test']
+        config = Toolbox.testconfig()
+    if args.config:
+        # load config from file
+        with open(args.config, 'r') as stream:
+            config = ruamel.yaml.YAML().load(stream)
+    elif select.select([sys.stdin,],[],[],0.0)[0]:
+        # expect wheelhouse yaml structured metadata
+        config = ruamel.yaml.YAML().load(sys.stdin)
+
+    jobs = args.jobs if args.jobs else jobs
+    Toolbox.merge_dict(config, config_override)
+
+    # Make it happen
+    if config['engine'] == 'salt':
+        # trigge Salt wheel
+        SaltWheel(config, jobs=jobs).runner()
+    else:
+      pass
+
+    #TODO, should we ignore errors?
+    #RED_ERROR = termcolor.colored('FATAL ERROR:', 'red')
+    #if args.debug:
+    #else:
+    #    try:
+    #        run(args)
+    #    except (errors.UserException, errors.BuildError) as exc:
+    #        print(RED_ERROR, exc.args[0], file=sys.stderr)
+    #        sys.exit(exc.CODE)
